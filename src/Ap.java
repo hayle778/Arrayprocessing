a6085310a75ed1b8b694333099638b67572079d4
yourUsername <yourEmail@domain.com>
2019-10-27 00:53:52 -0400

    https://github.com/hayle778/Arrayprocessing.git

---
+++ src/Ap.java
@@ -0,0 +1,28 @@
+
+/*
+  . we will make ready the class
+  . we are going to create the type of the array.
+  . we will declare he array of the integers, string, numbers etc
+  . we will prompt the user  and we add all the inputs of what we have to add
+  . we have to select a variable that can represent an int or double etc.
+  . we will include all the random and user numbers after initalizing it
+  . we will declare what we initalize it
+  . we will print out the program which is the sum and the average
+  . we will set out the program based on the users ask
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
